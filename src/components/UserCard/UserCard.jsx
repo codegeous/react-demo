c3cc333e82a57a08081f278701a92acf299d4f97
Prem Kumar <premkumarssp@gmail.com>
2020-01-20 20:58:13 +0400

    File upload progress bar with axios

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -16,9 +16,50 @@
 
 */
 import React, { Component } from "react";
+import {ProgressBar} from 'react-bootstrap';
+import axios from 'axios';
 
 export class UserCard extends Component {
+
+  state = {
+    uploadPercentage: 0,
+    avatar: ''
+  }
+
+  componentDidMount = () =>{
+    const {avatar} = this.props;
+    this.setState({ avatar })
+  }
+
+  uploadFile = ({ target: { files } }) =>{
+    console.log( files[0] )
+    let data = new FormData();
+    data.append( 'file', files[0] )
+
+    const options = {
+      onUploadProgress: (progressEvent) => {
+        const {loaded, total} = progressEvent;
+        let percent = Math.floor( (loaded * 100) / total )
+        console.log( `${loaded}kb of ${total}kb | ${percent}%` );
+
+        if( percent < 100 ){
+          this.setState({ uploadPercentage: percent })
+        }
+      }
+    }
+
+    axios.post("https://www.mocky.io/v2/5cc8019d300000980a055e76", data, options).then(res => { 
+        console.log(res)
+        this.setState({ avatar: res.data.url, uploadPercentage: 100 }, ()=>{
+          setTimeout(() => {
+            this.setState({ uploadPercentage: 0 })
+          }, 1000);
+        })
+    })
+  }
+
   render() {
+    const {uploadPercentage} = this.state;
     return (
       <div className="card card-user">
         <div className="image">
@@ -29,9 +70,12 @@ export class UserCard extends Component {
             <a href="#pablo">
               <img
                 className="avatar border-gray"
-                src={this.props.avatar}
+                src={this.state.avatar}
                 alt="..."
               />
+              <input type="file" className="form-control profile-pic-uploader" onChange={this.uploadFile} />
+              { uploadPercentage > 0 && <ProgressBar now={uploadPercentage} active label={`${uploadPercentage}%`} /> }
+
               <h4 className="title">
                 {this.props.name}
                 <br />
